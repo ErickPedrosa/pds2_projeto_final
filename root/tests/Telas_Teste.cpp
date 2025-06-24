b466bd7ffc73d61444abc0a36c63c4b7c7eb8d8a
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2025-06-24 16:20:05 -0300

    Testes das telas prontos

--- root/tests/Telas_Teste.cpp
+++ root/tests/Telas_Teste.cpp
@@ -1,3 +1,5 @@
+/*
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../lib/doctest/doctest/doctest.h"
 #include <allegro5/allegro.h>
@@ -5,7 +7,6 @@
 #include "../include/TelaInicial.hpp"
 #include "../include/TelaCadastro.hpp"
 #include "../include/TelaPlacar.hpp"
-#include "../include/TelaDeJogo.hpp"
 #include "../include/TelaGameOver.hpp"
 
 #define TELA_INICIO 0
@@ -15,12 +16,6 @@
 #define TELA_SAIR -1
 #define TELA_GAMEOVER 4
 
-/*
-TelaCadastro* telaCadastro = new TelaCadastro(al_get_display_height(disp), al_get_display_width(disp));
-TelaPlacar* telaPlacar = new TelaPlacar(al_get_display_height(disp), al_get_display_width(disp));
-TelaDeJogo* telaJogo = new TelaDeJogo();
-TelaGameOver* telaGameOver = new TelaGameOver(al_get_display_height(disp), al_get_display_width(disp));
-*/
 
 TEST_CASE("Testando a função de verificar clique nos botões (Tela Inicial)") {
 	REQUIRE(al_init());
@@ -32,11 +27,11 @@ TEST_CASE("Testando a função de verificar clique nos botões (Tela Inicial)") {
 
 
 
-	CHECK(telaInicio->VerificaClique(670, 341) == TELA_CADASTRO);
-	CHECK(telaInicio->VerificaClique(668, 436) == TELA_PLACAR);
-	CHECK(telaInicio->VerificaClique(638, 513) == TELA_SAIR);
-	CHECK(telaInicio->VerificaClique(200, 700) == TELA_INICIO);
-	CHECK(telaInicio->VerificaClique(800, 100) == TELA_INICIO);
+	CHECK(telaInicio->VerificaClique(670, 341) == TELA_CADASTRO); //Coordenadas do botão jogar
+	CHECK(telaInicio->VerificaClique(668, 436) == TELA_PLACAR); //Coordenadas do botão placar
+	CHECK(telaInicio->VerificaClique(638, 513) == TELA_SAIR); //Coordenadas do botão sair
+	CHECK(telaInicio->VerificaClique(200, 700) == TELA_INICIO); //Coordenadas da tela sem botão 
+	CHECK(telaInicio->VerificaClique(800, 100) == TELA_INICIO); //Coordenadas da tela sem botão
 	
 
 
@@ -45,11 +40,74 @@ TEST_CASE("Testando a função de verificar clique nos botões (Tela Inicial)") {
 }
 
 
-/*
+TEST_CASE("Testando a função de verificar clique nos botões (Tela Cadastro)") {
+	REQUIRE(al_init());
 
-delete(telaCadastro);
-delete(telaPlacar);
-delete(telaJogo);
-delete telaGameOver;
+	ALLEGRO_DISPLAY* disp = al_create_display(1280, 800);
+	REQUIRE(disp != nullptr);
+
+	TelaCadastro* telaCadastro = new TelaCadastro(al_get_display_height(disp), al_get_display_width(disp));
+
+
+
+	CHECK(telaCadastro->VerificaClique(577, 433) == TELA_JOGO); //Coordenadas do botão jogar
+	CHECK(telaCadastro->VerificaClique(668, 527) == TELA_INICIO); //Coordenadas do botão voltar
+	CHECK(telaCadastro->VerificaClique(50, 50) == TELA_CADASTRO); //Coordenadas da tela sem botão
+	CHECK(telaCadastro->VerificaClique(200, 700) == TELA_CADASTRO); //Coordenadas da tela sem botão 
+	CHECK(telaCadastro->VerificaClique(800, 100) == TELA_CADASTRO); //Coordenadas da tela sem botão
+
+
+
+
+	delete(telaCadastro);
+	al_destroy_display(disp);
+}
+
+
+TEST_CASE("Testando a função de verificar clique nos botões (Tela Placar)") {
+	REQUIRE(al_init());
+
+	ALLEGRO_DISPLAY* disp = al_create_display(1280, 800);
+	REQUIRE(disp != nullptr);
+
+	TelaPlacar* telaPlacar = new TelaPlacar(al_get_display_height(disp), al_get_display_width(disp));
+
+
+
+	CHECK(telaPlacar->VerificaClique(640, 635) == TELA_INICIO); //Coordenadas do botão jogar
+	CHECK(telaPlacar->VerificaClique(2000, 1000) == TELA_PLACAR); //Coordenadas da tela sem botão 
+	CHECK(telaPlacar->VerificaClique(200, 700) == TELA_PLACAR); //Coordenadas da tela sem botão 
+	CHECK(telaPlacar->VerificaClique(800, 100) == TELA_PLACAR); //Coordenadas da tela sem botão
+
+
+
+	delete(telaPlacar);
+	al_destroy_display(disp);
+}
+
+
+TEST_CASE("Testando a função de verificar clique nos botões (Tela Game-Over)") {
+	REQUIRE(al_init());
+
+	ALLEGRO_DISPLAY* disp = al_create_display(1280, 800);
+	REQUIRE(disp != nullptr);
+
+	TelaGameOver* telaGameOver = new TelaGameOver(al_get_display_height(disp), al_get_display_width(disp));
+
+
+
+	CHECK(telaGameOver->VerificaClique(714, 485) == TELA_JOGO); //Coordenadas do botão reiniciar
+	CHECK(telaGameOver->VerificaClique(645, 583) == TELA_INICIO); //Coordenadas do botão voltar para o inicio
+	CHECK(telaGameOver->VerificaClique(50, 650) == TELA_PLACAR); //Coordenadas da tela sem botão 
+	CHECK(telaGameOver->VerificaClique(800, 100) == TELA_PLACAR); //Coordenadas da tela sem botão
+	CHECK(telaGameOver->VerificaClique(10, 1000) == TELA_PLACAR); //Coordenadas da tela sem botão
+
+
+
+	delete(telaGameOver);
+	al_destroy_display(disp);
+}
+
+
 */
 
