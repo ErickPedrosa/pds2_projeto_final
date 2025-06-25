12ec8b3943a48c9e7c50f496ff34384bd00192ed
Lucas Maciel <freitaslucasmaciel@gmail.com>
2025-06-24 23:38:39 -0300

    commit final

--- root/src/Main.cpp
+++ root/src/Main.cpp
@@ -30,6 +30,8 @@
 #define TELA_PAUSE 5
 
 int main(int argc, char** argv) {
+	
+	//Inicio do Allegro
 	al_init();
 	al_install_keyboard();
 	al_install_mouse();
@@ -38,6 +40,7 @@ int main(int argc, char** argv) {
 	al_init_acodec_addon();
 	al_reserve_samples(2);
 
+	//Inicialização dos módulos do Allegro
 	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
 	ALLEGRO_EVENT_QUEUE* filaEventos = al_create_event_queue();
 	ALLEGRO_DISPLAY* disp = al_create_display(1280, 800);
@@ -45,6 +48,7 @@ int main(int argc, char** argv) {
 	ALLEGRO_SAMPLE* som_pulo = al_load_sample("./assets/wingflap.wav");
 	ALLEGRO_AUDIO_STREAM* som_fundo = al_load_audio_stream("./assets/musica_fundo.wav", 4, 2048);
 
+	
 	al_set_audio_stream_playmode(som_fundo, ALLEGRO_PLAYMODE_LOOP);
 	al_attach_audio_stream_to_mixer(som_fundo, al_get_default_mixer());
 
@@ -56,16 +60,19 @@ int main(int argc, char** argv) {
 	ALLEGRO_COLOR corTexto = al_map_rgb(255, 255, 204);
 	ALLEGRO_COLOR sombra = al_map_rgb(50, 50, 50);
 
+	//Variáveis para timer na Tela de Jogo
 	double tempoInicialPartida = 0.0;
 	double tempoPausado = 0.0;
 	bool entrouNaTelaDeJogo = false;
 	bool entrouNaTelaDePausa = false;
 
+	//Registrando eventos na pilha
 	al_register_event_source(filaEventos, al_get_keyboard_event_source());
 	al_register_event_source(filaEventos, al_get_display_event_source(disp));
 	al_register_event_source(filaEventos, al_get_timer_event_source(timer));
 	al_register_event_source(filaEventos, al_get_mouse_event_source());
 
+	//Inicializando objetos
 	TelaInicial* telaInicio = new TelaInicial(al_get_display_height(disp), al_get_display_width(disp));
 	TelaCadastro* telaCadastro = new TelaCadastro(al_get_display_height(disp), al_get_display_width(disp));
 	TelaPlacar* telaPlacar = new TelaPlacar(al_get_display_height(disp), al_get_display_width(disp));
@@ -75,18 +82,24 @@ int main(int argc, char** argv) {
 	Colisao* colidir = new Colisao();
 	GerenciadorCadastro* cadastroJogadores = new GerenciadorCadastro();
 
+	//Variáveis de controle do jogo
 	bool jogando = true;
 	bool pulo = false;
 	int telaAtual = 0;
 	int scoreAtual = 0;
 
+	//Inicio do timer do jogo
 	al_start_timer(timer);
 
+	//Inicio do looping do jogo
 	while (jogando) {
+		//Espera por um evento na fila
 		al_wait_for_event(filaEventos, &evento);
 
+		//Acaba com o programa 
 		if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) break;
 
+		//Verifica envento de mouse
 		if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
 			int _x = evento.mouse.x;
 			int _y = evento.mouse.y;
@@ -102,6 +115,7 @@ int main(int argc, char** argv) {
 			case TELA_CADASTRO:
 				telaDestino = telaCadastro->VerificaClique(_x, _y);
 				if (telaDestino == TELA_JOGO)
+					//Efetua o login do jogador
 					cadastroJogadores->Logar(telaCadastro->GetInput());
 				break;
 			case TELA_GAMEOVER:
@@ -118,6 +132,7 @@ int main(int argc, char** argv) {
 			telaCadastro->VerificaDigitacao(evento);
 		}
 
+		//Verifica envento de teclas pressionadas
 		if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch (evento.keyboard.keycode) {
 			case ALLEGRO_KEY_UP:
@@ -130,6 +145,7 @@ int main(int argc, char** argv) {
 			}
 		}
 
+		//Verifica evento de soltar teclas (Quando se solta a tecla de pulo)
 		if (evento.type == ALLEGRO_EVENT_KEY_UP) {
 			pulo = false;
 		}
@@ -137,6 +153,7 @@ int main(int argc, char** argv) {
 		if (evento.type == ALLEGRO_EVENT_TIMER) {
 			auto topJogadores = cadastroJogadores->TopNJogadores(5);
 
+			//Condicionais para verificar qual tela deve ser mostrada
 			switch (telaAtual) {
 			case TELA_INICIO:
 				al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -227,6 +244,7 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	//Desalocação dos objetos
 	delete telaInicio;
 	delete telaCadastro;
 	delete telaPlacar;
@@ -236,6 +254,8 @@ int main(int argc, char** argv) {
 	delete cadastroJogadores;
 	delete telaGameOver;
 
+
+	//Destruição dos módulos do Allegro
 	al_destroy_font(fonte);
 	al_destroy_display(disp);
 	al_destroy_timer(timer);
